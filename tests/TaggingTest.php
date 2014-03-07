519090505377b0cd62b05f3aca6eab17158f3e09
rtconner <rtconner@gmail.com>
2014-03-07 16:37:17 -0500

    rework testing to use a stub better

---
+++ tests/TaggingTest.php
@@ -0,0 +1,74 @@
+<?php namespace Conner\Tagging\Tests;
+
+use Conner\Tagging\Taggable;
+use Conner\Tagging\Tag;
+use Illuminate\Support\Facades\Config;
+
+class TaggingTest extends \TestCase {
+
+	public function setUp() {
+		\Illuminate\Foundation\Testing\TestCase::setUp();
+		
+		\Artisan::call('migrate', array('--package'=>'rtconner\laravel-tagging'));
+		
+		include_once(dirname(__FILE__).'\Stub.php');
+	}
+	
+	public function testTagging() {
+		$stub = $this->randomStub();
+	
+		$tagStrings = array('First Tag', '2nd Tag', 'Tag #3', 'tag #3', 'TAG #3');
+	
+		foreach($tagStrings as $tagString) {
+			$stub->tag($tagString);
+		}
+	
+		$this->assertEquals(count($tagStrings)-2, $stub->tagged->count());
+	}
+	
+	public function testUntag() {
+		$stub = $this->randomStub();
+	
+		$tagStrings = array('First Tag', '2nd Tag', 'Tag #3', 'TagX', 'TagY', 'TagZ');
+	
+		foreach($tagStrings as $tagString) {
+			$stub->tag($tagString);
+		}
+	
+		$stub->untag($tagStrings[0]);
+		$stub->untag($tagStrings[1]);
+		$stub->untag($tagStrings[2]);
+		$stub->untag($tagStrings[3]);
+	
+		$this->assertEquals(count($tagStrings)-4, $stub->tagged->count());
+	}
+	
+	public function testCreate() {
+		$tag = new Tag;
+		$tag->name = 'Some Tag';
+		$tag->save();
+	
+		$this->assertInternalType('string', $tag->slug);
+		$this->assertInternalType('int', $tag->id);
+	}
+
+	public function testInternational() {
+		$stub = $this->randomStub();
+		
+		$tagStrings = array('First Tag', '2nd Tag', 'Tag #3', 'TagX', 'TagY', 'TagZ');
+	
+		foreach($tagStrings as $tagString) {
+			$stub->tag($tagString);
+		}
+	
+		dd($stub->tagNames());
+	}
+	
+	private function randomStub() {
+		$stub = new TaggingStub;
+		$stub->id = rand(1,1000);
+	
+		return $stub;
+	}
+	
+}
