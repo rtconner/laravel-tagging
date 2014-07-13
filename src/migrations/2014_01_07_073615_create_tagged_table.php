302beaa2b9f13c679c1f4853ac15943e60f544bb
rtconner <rtconner@gmail.com>
2014-07-13 18:43:46 -0400

    better indexing

--- src/migrations/2014_01_07_073615_create_tagged_table.php
+++ src/migrations/2014_01_07_073615_create_tagged_table.php
@@ -8,8 +8,8 @@ class CreateTaggedTable extends Migration {
 	public function up() {
 		Schema::create('tagging_tagged', function(Blueprint $table) {
 			$table->increments('id');
-			$table->string('taggable_id', 36);
-			$table->string('taggable_type', 255);
+			$table->string('taggable_id', 36)->index();
+			$table->string('taggable_type', 255)->index();
 			//$table->morphs('taggable');
 			$table->string('tag_name', 255);
 			$table->string('tag_slug', 255)->index();
