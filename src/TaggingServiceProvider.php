4f715c82c609b5e6a5a83ed8d10f997d70ce1c06
Robert Conner <rconner@circlebacklending.com>
2015-05-08 11:27:22 -0400

    Fix paths

--- src/TaggingServiceProvider.php
+++ src/TaggingServiceProvider.php
@@ -17,9 +17,9 @@ class TaggingServiceProvider extends ServiceProvider {
 	 */
 	public function boot() {
 		$this->publishes([
-			__DIR__.'/../../../config/tagging.php' => config_path('tagging.php'),
-			__DIR__.'/../../../migrations/2014_01_07_073615_create_tagged_table.php' => $this->app->databasePath('database/migrations/2014_01_07_073615_create_tagged_table.php'),
-			__DIR__.'/../../../migrations/2014_01_07_073615_create_tags_table.php' => $this->app->databasePath('database/migrations/2014_01_07_073615_create_tags_table.php'),
+			__DIR__.'/../config/tagging.php' => config_path('tagging.php'),
+			__DIR__.'/../migrations/2014_01_07_073615_create_tagged_table.php' => $this->app->databasePath('database/migrations/2014_01_07_073615_create_tagged_table.php'),
+			__DIR__.'/../migrations/2014_01_07_073615_create_tags_table.php' => $this->app->databasePath('database/migrations/2014_01_07_073615_create_tags_table.php'),
 		]);
 	}
 	
