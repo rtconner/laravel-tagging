66f120475dcbb989286ffebbd70ff4efda6524dc
Robert Conner <rtconner@smarter.bz>
2019-07-02 22:26:21 -0400

    Make create tag group more compatible with older laravel

--- src/Console/Commands/GenerateTagGroup.php
+++ src/Console/Commands/GenerateTagGroup.php
@@ -5,31 +5,19 @@ namespace Conner\Tagging\Console\Commands;
 use Conner\Tagging\TaggingUtility;
 use Conner\Tagging\Model\TagGroup;
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class GenerateTagGroup extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'tagging:create-group {group_name}';
+    protected $name = 'tagging:create-group';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'tagging:create-group {group}';
+
     protected $description = 'Create a laravel tag group';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        $groupName = $this->argument('group_name');
+        $groupName = $this->argument('group');
 
         $tagGroup = new TagGroup();
         $tagGroup->name = $groupName;
@@ -37,6 +25,13 @@ class GenerateTagGroup extends Command
 
         $tagGroup->save();
 
-        $this->info('Created tag group: ' . $groupName);
+        $this->info('Created tag group: '.$groupName);
+    }
+
+    protected function getArguments()
+    {
+        return [
+            ['group', InputArgument::REQUIRED, 'Name of the group to create.'],
+        ];
     }
 }
