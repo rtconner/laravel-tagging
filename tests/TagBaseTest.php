188d80f2ce7645e7778be18a45a3ce331f38078a
Mateus Junges <mateusf.junges@gmail.com>
2020-03-10 10:59:50 -0300

    Feature: add a short description to the tag

--- tests/TagBaseTest.php
+++ tests/TagBaseTest.php
@@ -15,4 +15,19 @@ class TagBaseTest extends BaseTestCase
         $this->assertEquals('Foobar', $tag->name);
         $this->assertEquals('foobar', $tag->slug);
     }
+
+    public function test_it_can_have_a_description()
+    {
+        $description = 'Fooobar test description';
+        $tag = new Tag([
+            'name' => 'foobar',
+            'description' => $description
+        ]);
+
+        $tag->save();
+
+        $this->assertEquals('Foobar', $tag->name);
+        $this->assertEquals('foobar', $tag->slug);
+        $this->assertEquals($description, $tag->description);
+    }
 }
